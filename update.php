85ea774d12fb5b039f214824f82025c1836ae20d
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2019-04-29 20:13:23 +0400

    showcase

--- update.php
+++ update.php
@@ -3,7 +3,7 @@ use infrajs\config\Config;
 use infrajs\catalog\Catalog;
 use infrajs\sequence\Sequence;
 
-
+/*
 $r = ['-catalog-range/layout.tpl','catalog-range']; //Способ передать переменную без создания ссылки
 Sequence::add(Catalog::$conf['dependencies'],[],$r[1]); //Добавлена массив в конфиг текущей сессии установки
 Sequence::add(Catalog::$conf['filterstpl'],[],$r[0]);
@@ -11,3 +11,4 @@ Sequence::add(Catalog::$conf['filterstpl'],[],$r[0]);
 
 Sequence::set(Config::$sys, ['catalog','dependencies'], Catalog::$conf['dependencies']); //Сохранено в sys конфиг для появления и в обычной сессии
 Sequence::set(Config::$sys, ['catalog','filterstpl'], Catalog::$conf['filterstpl']);
+*/
